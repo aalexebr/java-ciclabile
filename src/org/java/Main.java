db68044a2902b0450819c311069cf3f2460d5b9c
aalexebr <alex.ebr@icloud.com>
2023-11-17 15:58:37 +0100

    bonus complete

--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -9,12 +9,22 @@ public class Main {
 	public static void main(String[] args) {
 		int[] array = {1,2,3};
 		IntegerArray x = new IntegerArray(array);
-		System.out.println(x);
+//		System.out.println(x);
 //		System.out.println(x.getCounter());
-		for(int j =0; j<10;j++) {
-			System.out.println(x.hasElementsLeft());
-			System.out.println(x.getNextElement());
-		}
+//		for(int j =0; j<10;j++) {
+//			System.out.println(x.hasElementsLeft());
+//			System.out.println(x.getNextElement());
+//		}
+		x.addElement(0);
+		System.out.println(x);
+		
+		IntegerArray y = new IntegerArray();
+		y.addElement(1);
+		y.addElement(2);
+		y.addElement(0);
+//		System.out.println(y);
+//		System.out.println(y.hasElementsLeft());
+		
 
 		
 	}
--- src/org/java/pojo/IntegerArray.java
+++ src/org/java/pojo/IntegerArray.java
@@ -7,10 +7,11 @@ public class IntegerArray {
 	private int length;
 	private int[] arrayInt = new int[length];
 	private int counter = 0;
+	private int counter2 = 0;
 	
-//	public IntegerArray() {
-//		
-//	}
+	public IntegerArray() {
+		
+	}
 	
 	public IntegerArray(int[] array) {
 		setLength(array);
@@ -61,6 +62,32 @@ public class IntegerArray {
 			return true;
 		}
 		else return false;
+	}
+	
+	
+	public void addElement(int x) {
+//		System.out.println("origina"+length);
+		if(length == 0) {
+			int[] newArr = {x};
+		setLength(newArr);
+		setArrayInt(newArr);
+		}
+		else {
+			length++;
+			int[] newArr = new int[length];
+//			System.out.println("length"+length);
+			newArr[0]= arrayInt[0];
+			for(int j=0; j<length-1;j++) {
+				newArr[j]=arrayInt[j];
+			}
+			newArr[length-1]=x;
+//			setLength(newArr);
+			setArrayInt(newArr);
+//			System.out.println(Arrays.toString(newArr));
+//			System.out.println("second"+length);
+		}
+		
+		
 	}
 	
 	@Override
